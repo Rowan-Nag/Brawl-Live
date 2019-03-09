bcece1316af0671f6477ea2c5cfb72c1b5770f69
Rowan-Nag <rowan_nag@icloud.com>
2019-03-08 18:15:40 -0800

    Added "listToMap(list), feel free to add more cases to it.

--- game.js
+++ game.js
@@ -76,11 +76,11 @@ class Player{
     this.width = 50
     this.height =50
   }
-  
+
   draw(x, y){
-    ctx.imageSmoothingEnabled = false													
+    ctx.imageSmoothingEnabled = false
     ctx.drawImage(this.image,0, 128*this.frameX, 128, 128, x, y, this.width, this.height);
-    
+
   }
   incrementFrame(numFrames, delayAmount) {
     if (this.delay < delayAmount) {
@@ -90,14 +90,14 @@ class Player{
         this.frameX += 1;
       } else {
         this.frameX = 0;
-        this.delay = 0; 
+        this.delay = 0;
       }
     }
-  } 
-  
+  }
+
   move(){
     let xTemp = this.x+0, yTemp = this.y+0
-    
+
     if(keys.a in keysDown){
       this.x -= this.speed
       this.image = document.getElementById("p1Left")
@@ -118,23 +118,23 @@ class Player{
       this.image = document.getElementById("p1Down")
     }
     this.facing = Math.atan2(this.y-yTemp, this.x-xTemp)
-    
-    
+
+
     if(this.x < 0){
       this.x = 0
     }
     if(this.y < 0){
       this.y = 0
     }
-  
+
     if(this.y > (game.map[0].length-1)*game.tileSize){
-      
+
       this.y = (game.map[0].length-1)*game.tileSize
     }
     if(this.x > (game.map.length-1)*game.tileSize){
       this.x = (game.map.length-1)*game.tileSize
     }
-   
+
     if(this.x > WIDTH/2 && this.x < game.tileSize*game.map.length-WIDTH/2){
       game.cameraX = this.x
     }
@@ -153,47 +153,61 @@ class Game{
     this.sprites = []
     this.cameraX = WIDTH/2
     this.cameraY = HEIGHT/2
-    this.tileSize = 50 
+    this.tileSize = 50
   }
   setup(){
-    this.genMap(10, 10);
+    this.genNewMap(10, 10);
     setInterval(this.player1.incrementFrame(1,2), 100);
   }
   drawMap(){
     ctx.imageSmoothingEnabled = false
     let cameraMinX = Math.floor((this.cameraX-WIDTH/2)/this.tileSize);
     let cameraMinY = Math.floor((this.cameraY-WIDTH/2)/this.tileSize);
-    
+
     for(let i = cameraMinX; i < Math.min(this.map.length, cameraMinX+this.tileSize+1); i++){
       for(let j = cameraMinY; j < Math.min(this.map[i].length, cameraMinY+this.tileSize+1); j++){
         ctx.drawImage(this.map[i][j].image, this.map[i][j].x-this.cameraX+WIDTH/2, this.map[i][j].y-this.cameraY+HEIGHT/2, this.tileSize, this.tileSize)
       }
     }
   }
-  genMap(w, h){
+  genNewMap(w, h){
     let a = [];
- 
+
     for(let i = 0; i< w; i++){
       let b = [];
-     
+
       for(let j = 0; j<h; j++){
         b.push(new Tile(i*this.tileSize, j*this.tileSize, this.tileSize, this.tileSize,'floor'))
-        
-        
+
+
       }
       a.push(b)
-      
+
     }
-    
+
     this.map = a
   }
+  listToMap(tiles){
+    let a = 0, b = 0
+    for(let i = 0; i < tiles.length; i++){
+      for(let j = 0; j < tiles[i].length; j++){
+          switch(tiles[j][i]){
+            //Tile Class: X, Y, Width, Height, Type (same as image id in html)
+            case 0:
+            b.push(j*this.tileSize, i*this.tileSize, this.tileSize, this.tileSize, 'floor')
+          }
+      }
+    }
+  }
+
+
   drawSprites(){
     for(let i = 0; i < this.sprites.length; i++){
       this.sprites[i].draw();
     }
     this.player1.draw(this.player1.x-this.cameraX+WIDTH/2, this.player1.y-this.cameraY+HEIGHT/2)
   }
-  
+
   stateEngine(){
     switch(this.state){
       case 0:
@@ -204,7 +218,7 @@ class Game{
         this.drawSprites();
     }
   }
-  
+
 }
 
 var game = new Game();
