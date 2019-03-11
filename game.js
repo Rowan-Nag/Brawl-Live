89060cc8822013d5bed9ad0808d937bb7008f5fb
Atherna <48332289+Atherna@users.noreply.github.com>
2019-03-11 10:09:05 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,27 @@
 var canvas = document.getElementById('base');
+var context = canvas.getContext("2d");
 ctx = canvas.getContext('2d');
 WIDTH = canvas.width;
 HEIGHT = canvas.height;
 ctx.fillRect(0,0,WIDTH,HEIGHT)
 var ticks = 0
+var mouseX = 0
+var mouseY = 0
+
+window.addEventListener('mousemove', initiatePos, false);
+
+function initiatePos(e) {
+    var pos = getMousePos(canvas, e);
+}
+
+function getMousePos(canvas, evt){
+	var rect = canvas.getBoundingClientRect();
+    
+        mouseX = ((evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width)
+        mouseY = ((evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
+	
+		
+  }
 
 var keys = {down: 40,
             up: 38,
@@ -149,7 +167,8 @@ class menuButton{
 }
 
 class Player{
-  constructor(img){
+  constructor(img, basicAttackImg){
+    this.basicAttackImg = document.getElementById(basicAttackImg)
     this.image = document.getElementById(img)
     this.x = WIDTH/2;
     this.y = HEIGHT/2;
@@ -158,6 +177,12 @@ class Player{
     this.width = 32
     this.height =42
     this.frameX = 0
+	  
+    this.basicAttackX = 0
+    this.basicAttackDelay = 0
+    this.angle = 0
+	
+    this.delay = 0  
     this.effects = {
       walking:true,
     }
@@ -173,6 +198,47 @@ class Player{
     if(this.frameX >= numFrames){
       this.frameX -= numFrames
     }
+  }
+  
+  basicAttack(){
+	  
+
+	  
+	  if(keys.q in keysDown){
+		  console.log(this.x)
+		  ctx.translate(this.x + 16, this.y + 24);
+		  
+		  ctx.rotate( this.angle * Math.PI / 180)
+
+		  ctx.translate( - (((this.x + this.x + 40) / 2) + 0 ), - (((this.y + this.y + 40)/2) + 20));
+		  ctx.drawImage(this.basicAttackImg, (64*this.basicAttackX) + 16, 0, 40, 32, this.x, this.y, this.width + 16, this.height + 16)
+		  
+		  ctx.setTransform(1, 0, 0, 1, 0, 0);
+		  
+	  if (this.basicAttackDelay < 1) {
+		  this.basicAttackDelay += 1;
+		} else {
+		  if (this.basicAttackX < 7 ) {
+			this.basicAttackX += 1;
+		  } else {
+			this.basicAttackX = 0; 
+		  }
+			this.basicAttackDelay = 0
+		}
+		  
+
+	  
+  }
+}
+	
+  mouseAngle(){
+	  
+	 this.angle = (-((Math.atan2(this.x - mouseX, this.y - mouseY)*180) / 2))/1.5
+	  
+	  
+	  
+	  
+	
   }
 
   move(){
@@ -243,11 +309,12 @@ class Player{
       game.cameraY = this.y
     }
   }
+	
 }
 
 class Game{
   constructor(){
-    this.player1 = new Player("p1Idle")
+    this.player1 = new Player("p1Idle", "playerOneBasicAttack")
     this.state = 0
     this.map = []
     this.mapAdds = []
