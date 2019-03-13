00c1ea48e77ebb9000114aff233fa0396ff8a40c
Atherna <48332289+Atherna@users.noreply.github.com>
2019-03-12 19:23:33 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,29 +1,9 @@
 var canvas = document.getElementById('base');
-var context = canvas.getContext("2d");
 ctx = canvas.getContext('2d');
 WIDTH = canvas.width;
 HEIGHT = canvas.height;
 ctx.fillRect(0,0,WIDTH,HEIGHT)
 var ticks = 0
-var mouseX = 0
-var mouseY = 0
-
-
-window.addEventListener('mousemove', initiatePos, false);
-
-function initiatePos(e) {
-    var pos = getMousePos(canvas, e);
-}
-
-function getMousePos(canvas, evt){
-	var rect = canvas.getBoundingClientRect();
-    
-        mouseX = ((evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width)
-        mouseY = ((evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
-	
-		
-  }
-
 
 var keys = {down: 40,
             up: 38,
@@ -39,18 +19,32 @@ var keys = {down: 40,
             r: 82,},
     keysDown = {},
     frameRate = 1/60,
-    frameDelay = frameRate*1000
+    frameDelay = frameRate*1000,
+    totalMenuButtons = 0
+
+
+window.addEventListener('mousemove', initiatePos, false);
+
+function initiatePos(e) {
+      var pos = getMousePos(canvas, e);
+  }
+
+function getMousePos(canvas, evt){
+  	var rect = canvas.getBoundingClientRect();
+
+    mouseX = ((evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width)
+    mouseY = ((evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
+
+
+}
 
 
 addEventListener("keydown", function (e) {
   keysDown[e.keyCode] = true;
 }, false);
-
 addEventListener("keyup", function (e) {
   delete keysDown[e.keyCode];
 }, false);
-
-
 
 var requestInterval = function (fn, delay) {
   var requestAnimFrame = (function () {
@@ -73,25 +67,89 @@ var requestInterval = function (fn, delay) {
   return handle;
 };
 
+var mid = document.getElementById('middleP')
+
 var peer = new Peer;
-var peerId, conn, connections = [];
+var peerId, mainConn, connections = [], hosting = false, connPosition = -2;
+
+
 peer.on('open', function(id) {
   console.log('My peer ID is: ' + id);
   peerId = id
 });
+
+
 peer.on('connection', function(conn){
-  console.log('hosting ', conn.id)
-  connections.push(conn)
-  conn.on('data', function(data) {
-    console.log('recieved ', data, ' from ', conn.id)
-  });
-})
+  console.log('connected to ', conn.id)
+  if(hosting){
+    console.log('hosting ', conn.id)
+  }
+  else if(!hosting){
+    console.log('hosted by ', conn.id)
+
+    }
+  conn.on('data', function(data){
+    //console.log('recieved ', data, ' from ', conn.id)
+    switch(data[0]){
+    case 0:
+		
+		mid.innerHTML=data[1].toString();
+		
+		break;
+
+    case 1:
+      mainConn=connectToId(data[1])
+      mid.innerHTML = "Both Players Have Been Connected!"
+      break;
+
+    case 2:
+      mainConn.send([4, game.player1.num])
+      startGame()
+      break;
+
+    case 3:
+      game.p2Keys = data[1]
+      break;
+    case 4:
+      this.player2 = new Player(data[1])
+      break;
+    case 5:
+      game.player2.x = data[1][0]
+      game.player2.y = data[1][1]
+      game.player2.image = data[1][2]
 
 
-function connectToId(id){
-  return peer.connect(id)
+      game.player1.x = data[2][0]
+      game.player1.y = data[2][1]
+      game.player1.image = data[2][2]
+  }
+  })
+});
+
+function startGame(){
+
+  if(hosting){
+    game.switchState(1)
+  }else{
+
+    game.switchState(2)
+  }
+  mainConn.send([4, game.player1.num])
+
+
 }
 
+function sendData(data, recievers){
+  for(let i = 0; i < recievers.length; i++){
+    recievers[i].send(data)
+
+  }
+}
+
+function connectToId(id){
+  let tempConn = peer.connect(id)
+  return tempConn
+}
 
 function collision(a, b){
   return (a.x < b.x + b.width &&
@@ -99,6 +157,96 @@ function collision(a, b){
    a.y < b.y + b.height &&
    a.y + a.height > b.y)
 }
+
+function addText(string, x, y, size, color){
+  ctx.font = size+"px Arial";
+  ctx.fillStyle = color;
+  return ctx.strokeText(string, x, y);
+}
+
+
+//Button functions
+function hostButton(){
+    hosting = true
+    console.log('HOSTING')
+    mid.innerHTML = "Your ID: " + peerId.toString();
+    game.switchMenu(game.menus.host)
+}
+
+function joinButton(){
+	console.log("JOIN")
+	document.getElementById("peerId").style.visibility = "visible";
+  hosting = false
+    game.switchMenu(game.menus.join)
+	
+}
+
+function startButton(){
+  if(!game.player2){
+    mainConn.send(2)
+	
+  }
+  else{
+	  mid.innerHTML = "No one has connected yet..."
+  }
+  mainConn.send([2]);
+  startGame();
+  game.switchState(1)
+  mid.innerHTML = ""
+  
+  
+
+}
+
+function readyButton(){
+	
+	mainConn.send([1,peerId])
+}
+
+function connectButton(){
+  
+  console.log("connectButton")
+  
+  document.getElementById("peerId").style.visibility = "hidden";
+
+  mainConn = connectToId(document.getElementById('peerId').value)
+  mainConn.send(1)
+  mainConn.send([1, peerId])
+  console.log(mainConn,mainConn.id, peerId)
+  game.switchMenu(game.menus.ping)
+  hosting = false
+  mid.innerHTML = ""
+  
+  
+	
+  
+  
+  
+  
+}
+
+function singlePlayerButton(){
+  game.switchState(3)
+}
+
+function changeChar(num){
+  
+  game.player1 = new Player(num)
+  game.switchMenu(game.menus.main)
+}
+
+function characterSelect(){
+	game.switchMenu(game.menus.charSelect)
+
+	
+}
+
+function howToPlay(){
+
+	game.switchMenu(game.menus.howToPlay)
+}
+
+
 class Tile{
   constructor(x, y, width, height, type, collision){
     this.x = x;
@@ -110,14 +258,49 @@ class Tile{
   }
 }
 
+class menuButton{
+  constructor(x, y, width, height, text, func, font, size){
+    this.x = x;
+    this.y = y;
+    this.text = document.createTextNode(text);
+    this.width = width;
+    this.height = height;
+    this.func = func;
+    this.button = document.createElement('button');
+    this.button.appendChild(this.text);
+    this.button.style.left = x.toString()+"px";
+    this.button.style.bottom = (HEIGHT-y).toString()+"px";
+    this.button.style.width = this.width.toString() + "px";
+    this.button.style.height = this.height.toString() + "px";
+    this.button.classList.add('menuButton')
+    this.image = document.getElementById('Nbutton')
+    this.buttonNum = totalMenuButtons;
+    this.button.onclick=func
+    console.log(this.button)
+    totalMenuButtons++
+    this.deactivate();
+    document.getElementById('canvasDiv').appendChild(this.button)
+    document.getElementById('canvasDiv').appendChild(document.createElement('p'))
+  }
+  draw(x, y){
+    ctx.drawImage(this.image, x, this.y+5+this.buttonNum*64, this.width, this.height)
+  }
+  activate(){
+    this.button.disabled = false;
+    this.button.style.zIndex = 2;
+  }
+  deactivate(){
+    this.button.disabled = true;
+    this.button.style.zIndex = -2;
+  }
 
+}
 
-
-
-class Player{	
-  constructor(img, basicAttackImg){
-	this.basicAttackImg = document.getElementById(basicAttackImg)
-    this.image = document.getElementById(img)
+class Player{
+  constructor(num, basicAttackImage){
+    this.num = num
+    this.basicAttackImg = playerData.images[num].attack
+    this.image = playerData.images[num].down
     this.x = WIDTH/2;
     this.y = HEIGHT/2;
     this.speed = 4
@@ -125,12 +308,20 @@ class Player{
     this.width = 32
     this.height = 42
     this.frameX = 0
-	  
-	this.basicAttackX = 0
-	this.basicAttackDelay = 0
-	this.angle = 0
-	  
-	this.delay = 0
+
+    this.downImg=playerData.images[num].down
+    this.upImg=playerData.images[num].up
+    this.leftImg=playerData.images[num].left
+    this.rightImg=playerData.images[num].right
+    console.log(this.downImg, 500)
+
+
+    this.basicAttackX = 0
+
+    this.basicAttackDelay = 0
+    this.angle = 0
+
+    this.delay = 0
     this.effects = {
       walking:true,
     }
@@ -138,10 +329,9 @@ class Player{
 
   draw(x, y){
     ctx.imageSmoothingEnabled = false
-    ctx.drawImage(this.image,32*this.frameX, 0, 32, 42, x, y, this.width, this.height);
+    ctx.drawImage(document.getElementById(this.image),32*this.frameX, 0, 32, 42, x, y, this.width, this.height);
 
   }
-	
   incrementFrame(numFrames) {
     this.frameX += 1
     if(this.frameX >= numFrames){
@@ -149,25 +339,69 @@ class Player{
     }
   }
 
-  move(){
+  basicAttack(){
+
+
+
+	  if(keys.q in keysDown){
+		  console.log(this.x)
+		  ctx.translate(this.x + 16, this.y + 24);
+
+		  ctx.rotate( this.angle * Math.PI / 180)
+
+		  ctx.translate( - (((this.x + this.x + 40) / 2) + 0 ), - (((this.y + this.y + 40)/2) + 20));
+		  ctx.drawImage(document.getElementById(this.basicAttackImg), (64*this.basicAttackX) + 16, 20, 64, 64, this.x, this.y, this.width + 32, this.height + 32)
+
+		  ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+	  if (this.basicAttackDelay < 1) {
+		  this.basicAttackDelay += 1;
+		} else {
+		  if (this.basicAttackX < 7 ) {
+			this.basicAttackX += 1;
+		  } else {
+			this.basicAttackX = 0;
+		  }
+			this.basicAttackDelay = 0
+		}
+
+
+
+  }
+}
+
+  mouseAngle(){
+
+	 this.angle = (-((Math.atan2(this.x - mouseX, this.y - mouseY)*180) / 2))/1.5
+
+
+
+
+
+  }
+
+  move(keyList){
     let xTemp = this.x+0, yTemp = this.y+0
-
-    if(keys.a in keysDown){
+    console.log(this.image)
+    if(keys.a in keyList){
       this.x -= this.speed
-      this.image = document.getElementById("p1Left")
+      this.image = this.leftImg
 
     }
-    if(keys.d in keysDown){
+
+    if(keys.d in keyList){
       this.x += this.speed
-      this.image = document.getElementById("p1Right")
+      this.image = this.rightImg
     }
-    if(keys.w in keysDown){
+
+    if(keys.w in keyList){
       this.y -= this.speed
-      this.image = document.getElementById("p1Up")
+      this.image = this.upImg
     }
-    if(keys.s in keysDown){
+
+    if(keys.s in keyList){
       this.y += this.speed
-      this.image = document.getElementById("p1Down")
+      this.image = this.downImg
     }
     let dy = this.y-yTemp
     let dx = this.x-xTemp
@@ -217,70 +451,60 @@ class Player{
       game.cameraY = this.y
     }
   }
-	 
-  basicAttack(){
-	  
 
-	  
-	  if(keys.q in keysDown){
-		  console.log(this.x)
-		  ctx.translate(this.x + 16, this.y + 24);
-		  
-		  ctx.rotate( this.angle * Math.PI / 180)
-
-		  ctx.translate( - (((this.x + this.x + 40) / 2) + 0 ), - (((this.y + this.y + 40)/2) + 20));
-		  ctx.drawImage(this.basicAttackImg, (64*this.basicAttackX) + 16, 0, 40, 32, this.x, this.y, this.width + 16, this.height + 16)
-		  
-		  ctx.setTransform(1, 0, 0, 1, 0, 0);
-		  
-	  if (this.basicAttackDelay < 1) {
-		  this.basicAttackDelay += 1;
-		} else {
-		  if (this.basicAttackX < 7 ) {
-			this.basicAttackX += 1;
-		  } else {
-			this.basicAttackX = 0; 
-		  }
-			this.basicAttackDelay = 0
-		}
-		  
-
-	  
-  }
 }
-	
-  mouseAngle(){
-	  
-	 this.angle = (-((Math.atan2(this.x - mouseX, this.y - mouseY)*180) / 2))/1.5
-	  
-	  
-	  
-	  
-	
-  }
-}
-
 
 class Game{
   constructor(){
-    this.player1 = new Player("p1Idle", "playerOneBasicAttack")
-    this.state = 1
-    this.map = []
-    this.mapAdds = []
-    this.mapCollision = []
-    this.sprites = []
-    this.cameraX = WIDTH/2
-    this.cameraY = HEIGHT/2
-    this.tileSize = 50
+    this.player1 = new Player(1)
+    this.player2 = new Player(1);
+    this.p2Keys = {};
+    this.state = 0;
+    this.map = [];
+    this.mapAdds = [];
+    this.mapCollision = [];
+    this.sprites = [];
+    this.cameraX = WIDTH/2;
+    this.cameraY = HEIGHT/2;
+    this.tileSize = 50;
+    this.menus = {};
+    this.currentMenu = [];
   }
   setup(){
     //this.genNewMap(10, 10);
 	  this.map = this.listToMap(MAPS[1].tiles)
     this.mapAdds = this.listToMap(MAPS[1].adds)
     this.mapCollision = this.renderCollision(MAPS[1].collision)
-
+    this.genMenus();
+    this.currentMenu = this.menus.main
+    this.switchMenu(this.currentMenu);
 
   }
+	
+  genMenus(){
+    //x, y, width, height, text, func, font, size
+    this.menus ={
+      main:[new menuButton(WIDTH/2-100, HEIGHT*1/2-200, 200, 50, 'How to Play', function(){howToPlay()}, 'arial', 15),
+		  	new menuButton(WIDTH/2-100, HEIGHT*1/2-175, 200, 50, 'Character select', function(){characterSelect()}, 'arial', 15),
+            new menuButton(WIDTH/2-100, HEIGHT*1/2-150, 200, 50, 'Solo Game', function(){singlePlayerButton()}, 'arial', 15),
+            new menuButton(WIDTH/2-100, HEIGHT*1/2-125, 200, 50, 'Host Game', function(){hostButton()}, 'arial', 15),
+            new menuButton(WIDTH/2-100, HEIGHT*1/2-100, 200, 50, 'Join Game', function(){joinButton()}, 'arial', 15)],
+		
+	  howToPlay:[new menuButton(WIDTH/2-100, HEIGHT*1/2-200, 200, 50, 'Back', function(){game.switchMenu(game.menus.main)}, 'arial', 15),
+				],
+		
+	  ping:[new menuButton(WIDTH/2-100, HEIGHT*1/2-200, 200, 50, 'Ready?', function(){mainConn.send([1,peerId])}, 'arial', 15)],
+		
+      host:[new menuButton(WIDTH/2-100, HEIGHT*1/2-225, 200, 50, 'Start Game', function(){startButton()}, 'arial', 15)],
+		
+      join:[new menuButton(WIDTH/2-100, HEIGHT*1/2-300, 200, 50, 'Connect', function(){connectButton()}, 'arial', 15)],
+		
+      charSelect:[new menuButton(50,- WIDTH + 100, 200, 50, 'P1', function(){changeChar(1)}, 'arial', 15),
+                  new menuButton(50,- WIDTH + 150, 200, 50, 'P2', function(){changeChar(2)}, 'arial', 15),]
+	  
+    }
+  }
+	
   drawMap(map){
     ctx.imageSmoothingEnabled = false
 
@@ -294,6 +518,7 @@ class Game{
       }
     }
   }
+
   renderCollision(tiles){
 
     let collideable = false,b = []
@@ -326,7 +551,7 @@ class Game{
     return b
 
   }
-	
+
   genNewMap(w, h){
     let a = [];
 
@@ -439,35 +664,95 @@ class Game{
       this.sprites[i].draw();
     }
     this.player1.draw(this.player1.x-this.cameraX+WIDTH/2, this.player1.y-this.cameraY+HEIGHT/2)
+    if(this.player2){
+    this.player2.draw(this.player2.x-this.cameraX+WIDTH/2, this.player2.y-this.cameraY+HEIGHT/2)
+  }
+  }
+
+  switchMenu(target){
+    for(let i = 0; i < this.currentMenu.length; i++){
+      this.currentMenu[i].deactivate();
+    }
+    for(let i = 0; i < target.length; i++){
+      target[i].activate();
+    }
+    this.currentMenu = target;
+  }
+
+  drawMenu(menu){
+    for(let i = 0;i< menu.length; i++){
+
+      menu[i].draw(menu[i].x, menu[i].y)
+
+    }
+
+  }
+
+  switchState(target){
+    ctx.clearRect(0,0,WIDTH,HEIGHT)
+    if(this.state == 0){
+      for(let i = 0; i< this.currentMenu.length; i++){
+        this.currentMenu[i].deactivate();
+      }
+    }
+    if(target == 0){
+      for(let i = 0; i< this.currentMenu.length; i++){
+        this.currentMenu = this.menus.main;
+        this.switchMenu(this.currentMenu);
+    }
+
+  }
+  this.state = target/1;
+}
+
+  renderOther(player){
+
   }
 
   stateEngine(){
+
     switch(this.state){
       case 0:
-
+        ctx.fillRect(0,0,WIDTH,HEIGHT)
+        this.drawMenu(this.currentMenu);
         break;
-      case 1:
-		this.player1.mouseAngle()
-        this.player1.move()
+      case 1: //hosting
+        this.player1.mouseAngle();
+        this.player1.move(keysDown);
+        this.player2.move(this.p2Keys)
         this.drawMap(this.map);
         this.drawMap(this.mapAdds);
         this.drawMap(this.mapCollision)
         this.drawSprites();
-		this.player1.basicAttack()
-		
+        this.player1.basicAttack();
+        mainConn.send([5, [this.player1.x,this.player1.y, this.player1.image],[this.player2.x,this.player2.y,this.player2.image]])
+        break;
+      case 2://joining
+        mainConn.send([3, keysDown])
+      case 3: //singlePlayer
+        this.player1.mouseAngle();
+        this.player1.move(keysDown);
+        this.drawMap(this.map);
+        this.drawMap(this.mapAdds);
+        this.drawMap(this.mapCollision)
+        this.drawSprites();
+        this.player1.basicAttack();
     }
   }
+
+
 
 }
 
 var game = new Game();
 game.setup();
+
 function update(){
   ctx.clearRect(0, 0, WIDTH, HEIGHT)
   game.stateEngine()
 }
-function updateTicks(){
 
+function updateTicks(){
   ticks += 1
   if(game.player1.effects.walking && ticks%2 ==0){
     game.player1.incrementFrame(2)
