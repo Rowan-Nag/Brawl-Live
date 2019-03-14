4ee9c6c29747a22b4efb070c66b55923e18430db
Rowan-Nag <rowan_nag@icloud.com>
2019-03-13 20:48:30 -0700

    attacks.js now has all attack info

--- playerAttacks.js
+++
@@ -1,6 +0,0 @@
-var playerAttacks = {
-  //x, y, attackImg, size scale, attack template
-  1:{
-    auto:function(x, y, rotation){return new Attack(x, y, "playerOneBasicAttack", 1, "melee1")}
-  }
-}
